482b085bd77b5c43b182e24c03eb34daf54c8de4
Dieter Baron <dillo@nih.at>
2019-11-09 09:52:27 +0100

    Initial Commit

---
+++ gfx-convert/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gfx-convert
+//
+//  Created by Dieter Baron on 2019/11/09.
+//  Copyright © 2019 T'Pau. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
